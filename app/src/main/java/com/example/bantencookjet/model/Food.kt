f82e8e83dfdf5700571c03028215427186a02552
fadlyoktapriadi <fadlyoktapriadi41@gmail.com>
2024-07-11 10:46:08 +0700

    add image food and dummyfood

---
+++ app/src/main/java/com/example/bantencookjet/model/Food.kt
@@ -0,0 +1,9 @@
+package com.example.bantencookjet.model
+
+data class Food(
+    val id: Long,
+    val image: Int,
+    val name: String,
+    val description: String,
+    val estimatePrice: String,
+)
---
+++ app/src/main/java/com/example/bantencookjet/model/FoodDataSource.kt
@@ -0,0 +1,27 @@
+package com.example.bantencookjet.model
+
+import com.example.bantencookjet.R
+
+object FoodDataSource {
+    val dummyFood = listOf(
+        Food(1, R.drawable.food1, "Sate Bandeng", "Sate Bandeng adalah masakan tradisional khas Banten, Indonesia. Sate Bandeng dibuat dari ikan bandeng  yang dihilangkan durinya, dagingnya dibumbui dan dimasukkan kembali ke kulitnya, lalu ditusuk atau dijepit tusukan tangkai bambu, lalu dibakar di atas bara arang. ", "Rp25.000 - Rp40.000"),
+        Food(2, R.drawable.food2, "Pecak Bandeng", "Pecak bandeng mulanya merupakan menu ikan bandeng yang dibakar lalu diberi sambal. Menu ini benar-benar gurih dengan tekstur daging yang lembut, dengan sensasi smokey dari proses pembakarannya.",
+        "Rp25.000 - Rp35.000"),
+        Food(3, R.drawable.food3, "Rabeg", "Rabeg adalah masakan tradisional yang berasal dari perpaduan bumbu khas Jazirah Arab dan Banten,yang terbuat dari daging kambing yang memakai bumbu sederhana. Rasanya gurih dan tidak terlalu berbahaya untuk yang menderita kolesterol karena makanan khas Jawa Serang ini tidak memakai santan dalam proses pengolahannya.",
+        "Rp20.000 - Rp30.000"),
+        Food(4, R.drawable.food4, "Nasi Gonjleng", "Nasi gonjleng merupakan masakan olahan nasi khas yang berasal dari sekitar wilayah Banten. Hampir disetiap Kota/Kabupaten di Banten dapat ditemui tradisi menu masakan tersebut. Bentuk dan rasanya mirip dan menyerupai nasi samin atau nasi kebuli khas Timur Tengah.  Menu ini diduga merupakan hasil perpaduan dua kebudayaan, Timur Tengah dan Nusantara khususnya Banten. Nasi gonjleng umumnya hanya tersaji pada hari-hari besar seperti Lebaran (Idul Fitri), Isra Mikraj, atau Maulid Nabi Muhammad bagi warga Banten.",
+        "Rp15.000 - Rp25.000"),
+        Food(5, R.drawable.food5, "Nasi Bakar Sumsum", "Nasi bakar sumsum, yang dikenal sebagai hidangan khas dari Serang, Banten.Hidangan ini berawal dari seorang tukang potong hewan yang merasa sisa-sisa tulang akan mubazir jika dibuang begitu saja dan membawanya pulang. Tulang yang ia bawa ini menarik perhatian seorang pembeli yang kemudian memecah tulang-tulang tersebut agar lebih mudah untuk dijual. Sisa bagian sumsum dari tulang-tulang itu diolah oleh istri pembeli, dan dicampurkan ke dalam nasi dan dibakar.Karena hasilnya ternyata enak, hal itu menyebar dari mulut ke mulut.",
+        "Rp20.000 - Rp30.000"),
+        Food(6, R.drawable.food6, "Angeun Lada", "Angeun lada adalah sayur kuah pedas khas Banten berisi daging dan jeroan sapi seperti jantung, limpa, babat, atau usus. Cara membuat angeun lada cukup tumis bumbu dan jeroan, lalu tuang kaldu dan masak sampai matang. Angeun lada yang hangat ini cocok disantap saat cuaca dingin.",
+        "Rp25.000 - Rp30.000"),
+        Food(7, R.drawable.food7, "Gerem Asem", "Gerem asem merupakan salah satu warisan budaya tak benda yang sudah ada sejak zaman kesultanan Banten. Kuliner yang bercitarasa pedas dan asam ini menjadi salah satu masakan favorit serta andalan masyarakat Banten khususnya kota Serang.",
+        "Rp20.000 - Rp30.000"),
+        Food(8, R.drawable.food8, "Ketan Bintul", "Ketan bintul adalah makanan khas Banten berbahan ketan dan taburan serundeng sejenis kelapa parut yang disangrai dengan rempah-rempah. Bisa pula disajikan dengan gulai kambing atau semur daging. Ketan bintul dapat dijumpai di pasar-pasar tradisional atau di toko-toko kue. Hidangan ini sering dijumpai sebagai menu pembuka puasa di bulan Ramadan.",
+        "Rp20.000 - Rp30.000"),
+        Food(9, R.drawable.food9, "Balok Menes",  "Balok menes memiliki bentuk dan rasa yang mirip getuk dengan tambahan serundeng dan bawang goreng. Salah satu yang unik dari balok menes adalah cara membungkusnya yang menggunakan daun pisang sehingga menimbulkan aroma harum dan lembut yang membuatnya disukai banyak orang.",
+        "Rp20.000 - Rp30.000"),
+        Food(10, R.drawable.food10, "Sate Bebek", "sate bebek Cibeber sudah terasa enaknya walau belum diberi bumbu saji. Hal ini menjadikan sate bebek Cibeber sering kali dimakan tanpa bumbu saji dan tanpa lontong/nasi/ketupat. Ada kesan kenyal, manis dan gurih, serta aroma  wangi, sekalipun sate baru diangkat dari perapian.",
+        "Rp20.000 - Rp30.000"),
+    )
+}
