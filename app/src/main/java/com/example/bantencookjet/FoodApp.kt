bd2855a0a60831b55293bc2336b57bdabd2a8f41
fadlyoktapriadi <fadlyoktapriadi41@gmail.com>
2024-07-11 16:15:02 +0700

    add design Food Item (error)

--- app/src/main/java/com/example/bantencookjet/FoodApp.kt
+++ app/src/main/java/com/example/bantencookjet/FoodApp.kt
@@ -1,4 +1,16 @@
 package com.example.bantencookjet
 
-class FoodApp {
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.bantencookjet.ui.theme.BantenCookJetTheme
+
+@Composable
+fun FoodApp(){}
+
+@Preview(showBackground = true)
+@Composable
+fun JetHeroesAppPreview() {
+    BantenCookJetTheme {
+        FoodApp()
+    }
 }
--- app/src/main/java/com/example/bantencookjet/ui/component/FoodItem.kt
+++ app/src/main/java/com/example/bantencookjet/ui/component/FoodItem.kt
@@ -1,2 +1,79 @@
 package com.example.bantencookjet.ui.component
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.bantencookjet.R
+import com.example.bantencookjet.ui.theme.BantenCookJetTheme
+
+@Composable
+fun FoodItem(
+    image: Int,
+    title: String,
+    desc: String,
+    estimatePrice: String,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Row(
+            modifier = modifier
+        ) {
+            Image(
+                painter = painterResource(image),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(MaterialTheme.shapes.extraLarge)
+            )
+            Column {
+                Text(
+                    text = title,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    style = MaterialTheme.typography.titleMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
+                Text(
+                    text = desc,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    style = MaterialTheme.typography.titleMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
+                Text(
+                    text = estimatePrice,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    style = MaterialTheme.typography.titleMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
+            }
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun FoodItemPreview() {
+    BantenCookJetTheme {
+        FoodItem(R.drawable.food1, "Jaket Hoodie Dicoding", "aasdfasdgsaas", "Rp331-Rp331")
+    }
+}
